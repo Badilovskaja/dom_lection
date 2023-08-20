5ba224e02f68beed546b958ea3e4f1850e6433ae
Olga Badzilouskaja <olga2978788@yandex.ru>
2023-08-20 13:52:21 +0300

    d

---
+++ less10/data.js
@@ -0,0 +1,26 @@
+// ............2--задание
+export const dataInfo = `
+    [
+        {
+            "id": "01",
+            "url": "https://irecommend.ru/sites/default/files/product-images/2682123/7vLRiCDg7QhfHglk4jJEtA.png",
+            "name": "Olga",
+            "surname": "Badilovskaja",
+            "age": "30"
+        },
+        {
+            "id": "99",
+            "url": "https://html5css.ru/w3images/avatar5.png",
+            "name": "Vika",
+            "surname": "Vasilyeva",
+            "age": "9"
+        },
+        {
+            "id": "150",
+            "url": "https://klike.net/uploads/posts/2019-03/1551511801_1.jpg",
+            "name": "Jan",
+            "surname": "Ivanov",
+            "age": "7"
+        }
+    ]
+`
